b8c510964d79e33bbdf36a5ed88e2bdcb1041766
Yu Yasuda <38551932+YasudaYu@users.noreply.github.com>
2020-12-22 22:04:53 +0900

    todo登録上限設定の実装

--- src/App.jsx
+++ src/App.jsx
@@ -42,7 +42,11 @@ export const App = () => {
         todoText={todoText}
         onChange={onChangeTodoText}
         onClick={onClickAdd}
+        disabled={incompleteTodos.length >= 5}
       />
+      {incompleteTodos.length >= 5 && (
+        <p style={{ color: "red" }}>登録できるtodoは5個まで、消化せい</p>
+      )}
       <IncompleteTodos
         incompleteTodos={incompleteTodos}
         onClickComplete={onClickComplete}
